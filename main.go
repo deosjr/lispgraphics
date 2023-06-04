3e69d84bc3b07ffcf1d8733ec20c314e84ea9aba
Sjoerd Dost <deosjr@gmail.com>
2023-06-04 16:37:57 +0200

    fix last instance of set! quote

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-    "math"
+	"math"
 
-    "github.com/deosjr/whistle/lisp"
+	"github.com/deosjr/whistle/lisp"
 	"github.com/faiface/pixel/pixelgl"
 )
 
@@ -12,36 +12,36 @@ func main() {
 }
 
 func run() {
-    l := lisp.New()
-    LoadPixel(l.Env)
-    l.Env.AddBuiltin("sin", func(args []lisp.SExpression) (lisp.SExpression, error) {
-        return lisp.NewPrimitive(math.Sin(args[0].AsNumber())), nil
-    })
-    l.Env.AddBuiltin("cos", func(args []lisp.SExpression) (lisp.SExpression, error) {
-        return lisp.NewPrimitive(math.Cos(args[0].AsNumber())), nil
-    })
+	l := lisp.New()
+	LoadPixel(l.Env)
+	l.Env.AddBuiltin("sin", func(args []lisp.SExpression) (lisp.SExpression, error) {
+		return lisp.NewPrimitive(math.Sin(args[0].AsNumber())), nil
+	})
+	l.Env.AddBuiltin("cos", func(args []lisp.SExpression) (lisp.SExpression, error) {
+		return lisp.NewPrimitive(math.Cos(args[0].AsNumber())), nil
+	})
 
-    l.Eval("(define win-w 1024)")
-    l.Eval("(define win-h 768)")
-    l.Eval("(define win (window))")
-    l.Eval("(define refresh 100)")
+	l.Eval("(define win-w 1024)")
+	l.Eval("(define win-h 768)")
+	l.Eval("(define win (window))")
+	l.Eval("(define refresh 100)")
 
-    // TODO hack! this isnt behaviour of LISP set!, this is overriding on toplevel always!
-    // Should either implement set! properly or pass turtle _everywhere_
-    // usage: (set! 'top 3)
-    //l.Eval("(define set! (lambda (s v) (eval (list 'define s (quasiquote (quote ,v))) env) ))")
-    //l.Eval("(define not (lambda (t) (if (eqv? t #t) #f #t)))")
+	// TODO hack! this isnt behaviour of LISP set!, this is overriding on toplevel always!
+	// Should either implement set! properly or pass turtle _everywhere_
+	// usage: (set! 'top 3)
+	//l.Eval("(define set! (lambda (s v) (eval (list 'define s (quasiquote (quote ,v))) env) ))")
+	//l.Eval("(define not (lambda (t) (if (eqv? t #t) #f #t)))")
 
-    // this is still pretty hacky...
-    l.Eval("(define env (environment))")
-    envexp, _ := l.Eval("env")
-    drawenv := envexp.AsPrimitive().(*lisp.Env)
-    oldenv := l.Env
-    l.Env = drawenv
-    loadTurtleGraphics(l)
-    l.Env = oldenv
+	// this is still pretty hacky...
+	l.Eval("(define env (environment))")
+	envexp, _ := l.Eval("env")
+	drawenv := envexp.AsPrimitive().(*lisp.Env)
+	oldenv := l.Env
+	l.Env = drawenv
+	loadTurtleGraphics(l)
+	l.Env = oldenv
 
-    l.Eval(`(define tick (lambda () #t))`)
+	l.Eval(`(define tick (lambda () #t))`)
 
 	l.Eval(`(define drawrec (lambda ()
         (receive
@@ -50,11 +50,10 @@ func run() {
             (after refresh -> (if (closed? win) #t
                 (begin (tick) (drawrec)))))))`)
 
-
-    // TODO: if drawpid dies and repl isnt killed, things are broken
-    // if repl is killed, things are still broken (old repl is still reading)
-    // fix: 'read' shouldnt be blocking somehow? -> havent managed to figure out how yet
-    // other fix: REPL sends to named pid!
+	// TODO: if drawpid dies and repl isnt killed, things are broken
+	// if repl is killed, things are still broken (old repl is still reading)
+	// fix: 'read' shouldnt be blocking somehow? -> havent managed to figure out how yet
+	// other fix: REPL sends to named pid!
 	l.Eval(`(define REPL (lambda (drawpid)
         (begin (display "> ")
             (send drawpid (quasiquote (input (unquote (read)))))
@@ -74,22 +73,22 @@ func run() {
 }
 
 func loadTurtleGraphics(l lisp.Lisp) {
-    // turtle properties
-    l.Eval("(define turtle-line-width 5)")
-    l.Eval("(define turtle-pen-colour red)")
-    l.Eval("(define turtle-pen-down #t)")
-    l.Eval("(define turtle-pos (cons (/ win-w 2) (/ win-h 2)))")
-    l.Eval("(define turtle-heading 0)")
+	// turtle properties
+	l.Eval("(define turtle-line-width 5)")
+	l.Eval("(define turtle-pen-colour red)")
+	l.Eval("(define turtle-pen-down #t)")
+	l.Eval("(define turtle-pos (cons (/ win-w 2) (/ win-h 2)))")
+	l.Eval("(define turtle-heading 0)")
 
-    l.Eval(`(define mod-fixed (lambda (n m)
+	l.Eval(`(define mod-fixed (lambda (n m)
         (if (> n 0) (mod n m) (mod (+ n m) m)))) `)
 
-    l.Eval(`(define turn (lambda (radians)
+	l.Eval(`(define turn (lambda (radians)
         (set! turtle-heading (mod-fixed (+ turtle-heading radians) (* 2 pi)))))`)
-    l.Eval("(define left (lambda () (turn (/ pi 2))))")
-    l.Eval("(define right (lambda () (turn (- 0 (/ pi 2)))))")
+	l.Eval("(define left (lambda () (turn (/ pi 2))))")
+	l.Eval("(define right (lambda () (turn (- 0 (/ pi 2)))))")
 
-    l.Eval(`(define forward (lambda (n) (let ((px (car turtle-pos)) (py (cdr turtle-pos)) (vx (cos turtle-heading)) (vy (sin turtle-heading)))
+	l.Eval(`(define forward (lambda (n) (let ((px (car turtle-pos)) (py (cdr turtle-pos)) (vx (cos turtle-heading)) (vy (sin turtle-heading)))
         (let ((newx (+ px (* n vx))) (newy (+ py (* n vy))))
             (let ((newpos (cons newx newy)))
                 (if (eqv? turtle-pen-down #t) (draw-line turtle-pos newpos))
@@ -97,11 +96,11 @@ func loadTurtleGraphics(l lisp.Lisp) {
                 (wraparound)
     )))))`)
 
-    l.Eval(`(define wraparound (lambda () (let ((px (car turtle-pos)) (py (cdr turtle-pos)))
+	l.Eval(`(define wraparound (lambda () (let ((px (car turtle-pos)) (py (cdr turtle-pos)))
         (set! turtle-pos (cons (mod-fixed px win-w) (mod-fixed py win-h)))
     )))`)
 
-    l.Eval(`(define draw-line (lambda (from to)
+	l.Eval(`(define draw-line (lambda (from to)
         (let ((imd (imdraw)))
             (im-set-color! imd turtle-pen-colour)
             (im-push imd (vec2d (car from) (cdr from)))
@@ -111,14 +110,14 @@ func loadTurtleGraphics(l lisp.Lisp) {
             (update win)
     )))`)
 
-    l.Eval("(define gosper3 (quote (f r f r r f l f l l f f l f r r l f r f f r r f r f l l f l f r r l f r f f r r f r f l l f l f l f r f r r f l f l l f f l f r l l f r f r r f l f l l f f l f r f r f r r f l f l l f f l f r l l f r f f r r f r f l l f l f r r l f r f r r f l f l l f f l f r r l f r f f r r f r f l l f l f l f r f f r r f r f l l f l f r r l f r f f r r f r f l l f l f r f r f r r f l f l l f f l f r l l f r f r r f l f l l f f l f r l l f r f f r r f r f l l f l f r r l f r f r r f l f l l f f l f r r l f r f f r r f r f l l f l f l f r f f r r f r f l l f l f r r l f r f f r r f r f l l f l f r f r f r r f l f l l f f l f r l l f r f r r f l f l l f f l f r l l f r f f r r f r f l l f l f l f r f r r f l f l l f f l f r r l f r f f r r f r f l l f l f r r l f r f f r r f r f l l f l f l f r f r r f l f l l f f l f r l l f r f r r f l f l l f f l f r f r f r r f l f l l f f l f r l l f r f f r r f r f l l f l f r l l f r f r r f l f l l f f l f r r l f r f f r r f r f l l f l f r r l f r f f r r f r f l l f l f l f r f r r f l f l l f f l f r l l f r f r r f l f l l f f l f r f r f r r f l f l l f f l f r l l f r f f r r f r f l l f l f r f r f r r f l f l l f f l f r r l f r f f r r f r f l l f l f r r l f r f f r r f r f l l f l f l f r f r r f l f l l f f l f r l l f r f r r f l f l l f f l f r f r f r r f l f l l f f l f r l l f r f f r r f r f l l f l f r l l f r f r r f l f l l f f l f r r l f r f f r r f r f l l f l f l f r f f r r f r f l l f l f r r l f r f f r r f r f l l f l f r f r f r r f l f l l f f l f r l l f r f r r f l f l l f f l f r l l f r f f r r f r f l l f l f r)))")
-    l.Eval("(define program (quote ()))")
+	l.Eval("(define gosper3 (quote (f r f r r f l f l l f f l f r r l f r f f r r f r f l l f l f r r l f r f f r r f r f l l f l f l f r f r r f l f l l f f l f r l l f r f r r f l f l l f f l f r f r f r r f l f l l f f l f r l l f r f f r r f r f l l f l f r r l f r f r r f l f l l f f l f r r l f r f f r r f r f l l f l f l f r f f r r f r f l l f l f r r l f r f f r r f r f l l f l f r f r f r r f l f l l f f l f r l l f r f r r f l f l l f f l f r l l f r f f r r f r f l l f l f r r l f r f r r f l f l l f f l f r r l f r f f r r f r f l l f l f l f r f f r r f r f l l f l f r r l f r f f r r f r f l l f l f r f r f r r f l f l l f f l f r l l f r f r r f l f l l f f l f r l l f r f f r r f r f l l f l f l f r f r r f l f l l f f l f r r l f r f f r r f r f l l f l f r r l f r f f r r f r f l l f l f l f r f r r f l f l l f f l f r l l f r f r r f l f l l f f l f r f r f r r f l f l l f f l f r l l f r f f r r f r f l l f l f r l l f r f r r f l f l l f f l f r r l f r f f r r f r f l l f l f r r l f r f f r r f r f l l f l f l f r f r r f l f l l f f l f r l l f r f r r f l f l l f f l f r f r f r r f l f l l f f l f r l l f r f f r r f r f l l f l f r f r f r r f l f l l f f l f r r l f r f f r r f r f l l f l f r r l f r f f r r f r f l l f l f l f r f r r f l f l l f f l f r l l f r f r r f l f l l f f l f r f r f r r f l f l l f f l f r l l f r f f r r f r f l l f l f r l l f r f r r f l f l l f f l f r r l f r f f r r f r f l l f l f l f r f f r r f r f l l f l f r r l f r f f r r f r f l l f l f r f r f r r f l f l l f f l f r l l f r f r r f l f l l f f l f r l l f r f f r r f r f l l f l f r)))")
+	l.Eval("(define program (quote ()))")
 
-    l.Eval(`(define start (lambda ()
+	l.Eval(`(define start (lambda ()
         (begin
         (set! program gosper3)
         (set! tick (lambda () (begin
-            (if (null? program) (set! 'program (quote (done))))
+            (if (null? program) (set! program (quote (done))))
             (let ((next (car program)) (rem (cdr program)))
                 (cond
                     ((eqv? next 'f) (forward 10))
--- pixel.go
+++ pixel.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-    "github.com/deosjr/whistle/lisp"
+	"github.com/deosjr/whistle/lisp"
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/imdraw"
 	"github.com/faiface/pixel/pixelgl"
@@ -10,34 +10,34 @@ import (
 // wrapper around faiface/pixel libraries for use in lisp
 
 func LoadPixel(env *lisp.Env) {
-    // window
-    env.AddBuiltin("window", newWindow)
-    env.AddBuiltin("closed?", isClosed)
-    env.AddBuiltin("clear", clear)
-    env.AddBuiltin("update", update)
+	// window
+	env.AddBuiltin("window", newWindow)
+	env.AddBuiltin("closed?", isClosed)
+	env.AddBuiltin("clear", clear)
+	env.AddBuiltin("update", update)
 
-    // imdraw
-    env.AddBuiltin("imdraw", newIMDraw)
-    env.AddBuiltin("im-set-color!", setColor)
-    env.AddBuiltin("im-push", push)
-    env.AddBuiltin("im-draw", drawIMDraw)
+	// imdraw
+	env.AddBuiltin("imdraw", newIMDraw)
+	env.AddBuiltin("im-set-color!", setColor)
+	env.AddBuiltin("im-push", push)
+	env.AddBuiltin("im-draw", drawIMDraw)
 
-    // vector
-    env.AddBuiltin("vec2d", newVector)
+	// vector
+	env.AddBuiltin("vec2d", newVector)
 
-    // colors
-    env.Add("black", lisp.NewPrimitive(pixel.RGB(0, 0, 0)))
-    env.Add("red",   lisp.NewPrimitive(pixel.RGB(1, 0, 0)))
-    env.Add("green", lisp.NewPrimitive(pixel.RGB(0, 1, 0)))
-    env.Add("blue",  lisp.NewPrimitive(pixel.RGB(0, 0, 1)))
+	// colors
+	env.Add("black", lisp.NewPrimitive(pixel.RGB(0, 0, 0)))
+	env.Add("red", lisp.NewPrimitive(pixel.RGB(1, 0, 0)))
+	env.Add("green", lisp.NewPrimitive(pixel.RGB(0, 1, 0)))
+	env.Add("blue", lisp.NewPrimitive(pixel.RGB(0, 0, 1)))
 
-    // shapes
-    env.AddBuiltin("line", line)
-    env.AddBuiltin("polygon", polygon)
+	// shapes
+	env.AddBuiltin("line", line)
+	env.AddBuiltin("polygon", polygon)
 
-    // canvas
-    env.AddBuiltin("canvas", newCanvas)
-    env.AddBuiltin("canvas-draw", drawCanvas)
+	// canvas
+	env.AddBuiltin("canvas", newCanvas)
+	env.AddBuiltin("canvas-draw", drawCanvas)
 }
 
 func newWindow(args []lisp.SExpression) (lisp.SExpression, error) {
@@ -47,94 +47,94 @@ func newWindow(args []lisp.SExpression) (lisp.SExpression, error) {
 		VSync:  true,
 	}
 	win, err := pixelgl.NewWindow(cfg)
-    return lisp.NewPrimitive(win), err
+	return lisp.NewPrimitive(win), err
 }
 
 func isClosed(args []lisp.SExpression) (lisp.SExpression, error) {
-    win := args[0].AsPrimitive().(*pixelgl.Window)
-    return lisp.NewPrimitive(win.Closed()), nil
+	win := args[0].AsPrimitive().(*pixelgl.Window)
+	return lisp.NewPrimitive(win.Closed()), nil
 }
 
 func clear(args []lisp.SExpression) (lisp.SExpression, error) {
-    win := args[0].AsPrimitive().(*pixelgl.Window)
-    color := args[1].AsPrimitive().(pixel.RGBA)
-    win.Clear(color)
-    return lisp.NewPrimitive(true), nil
+	win := args[0].AsPrimitive().(*pixelgl.Window)
+	color := args[1].AsPrimitive().(pixel.RGBA)
+	win.Clear(color)
+	return lisp.NewPrimitive(true), nil
 }
 
 func update(args []lisp.SExpression) (lisp.SExpression, error) {
-    win := args[0].AsPrimitive().(*pixelgl.Window)
-    win.Update()
-    return lisp.NewPrimitive(true), nil
+	win := args[0].AsPrimitive().(*pixelgl.Window)
+	win.Update()
+	return lisp.NewPrimitive(true), nil
 }
 
 func newIMDraw(args []lisp.SExpression) (lisp.SExpression, error) {
-    imd := imdraw.New(nil)
-    return lisp.NewPrimitive(imd), nil
+	imd := imdraw.New(nil)
+	return lisp.NewPrimitive(imd), nil
 }
 
 func setColor(args []lisp.SExpression) (lisp.SExpression, error) {
-    imd := args[0].AsPrimitive().(*imdraw.IMDraw)
-    color := args[1].AsPrimitive().(pixel.RGBA)
-    imd.Color = color
-    return lisp.NewPrimitive(true), nil
+	imd := args[0].AsPrimitive().(*imdraw.IMDraw)
+	color := args[1].AsPrimitive().(pixel.RGBA)
+	imd.Color = color
+	return lisp.NewPrimitive(true), nil
 }
 
 func push(args []lisp.SExpression) (lisp.SExpression, error) {
-    imd := args[0].AsPrimitive().(*imdraw.IMDraw)
-    vecs := []pixel.Vec{}
-    for _, arg := range args[1:] {
-        if !arg.IsPrimitive() {
-            break
-        }
-        v, ok := arg.AsPrimitive().(pixel.Vec)
-        if !ok {
-            break
-        }
-        vecs = append(vecs, v)
-    }
-    if len(vecs) == 0 {
-        return lisp.NewPrimitive(false), nil
-    }
-    imd.Push(vecs...)
-    return lisp.NewPrimitive(true), nil
+	imd := args[0].AsPrimitive().(*imdraw.IMDraw)
+	vecs := []pixel.Vec{}
+	for _, arg := range args[1:] {
+		if !arg.IsPrimitive() {
+			break
+		}
+		v, ok := arg.AsPrimitive().(pixel.Vec)
+		if !ok {
+			break
+		}
+		vecs = append(vecs, v)
+	}
+	if len(vecs) == 0 {
+		return lisp.NewPrimitive(false), nil
+	}
+	imd.Push(vecs...)
+	return lisp.NewPrimitive(true), nil
 }
 
 func drawIMDraw(args []lisp.SExpression) (lisp.SExpression, error) {
-    imd := args[0].AsPrimitive().(*imdraw.IMDraw)
-    target := args[1].AsPrimitive().(pixel.Target)
-    imd.Draw(target)
-    return lisp.NewPrimitive(true), nil
+	imd := args[0].AsPrimitive().(*imdraw.IMDraw)
+	target := args[1].AsPrimitive().(pixel.Target)
+	imd.Draw(target)
+	return lisp.NewPrimitive(true), nil
 }
 
 func newVector(args []lisp.SExpression) (lisp.SExpression, error) {
-    x, y := args[0].AsNumber(), args[1].AsNumber()
-    return lisp.NewPrimitive(pixel.V(x,y)), nil
+	x, y := args[0].AsNumber(), args[1].AsNumber()
+	return lisp.NewPrimitive(pixel.V(x, y)), nil
 }
 
 func line(args []lisp.SExpression) (lisp.SExpression, error) {
-    imd := args[0].AsPrimitive().(*imdraw.IMDraw)
-    thickness := args[1].AsNumber()
-    imd.Line(thickness)
-    return lisp.NewPrimitive(true), nil
+	imd := args[0].AsPrimitive().(*imdraw.IMDraw)
+	thickness := args[1].AsNumber()
+	imd.Line(thickness)
+	return lisp.NewPrimitive(true), nil
 }
 
 func polygon(args []lisp.SExpression) (lisp.SExpression, error) {
-    imd := args[0].AsPrimitive().(*imdraw.IMDraw)
-    thickness := args[1].AsNumber()
-    imd.Polygon(thickness)
-    return lisp.NewPrimitive(true), nil
+	imd := args[0].AsPrimitive().(*imdraw.IMDraw)
+	thickness := args[1].AsNumber()
+	imd.Polygon(thickness)
+	return lisp.NewPrimitive(true), nil
 }
 
 func newCanvas(args []lisp.SExpression) (lisp.SExpression, error) {
-    windowBounds := pixel.R(0, 0, 1024, 768)
-    canvas := pixelgl.NewCanvas(windowBounds)
-    return lisp.NewPrimitive(canvas), nil
+	windowBounds := pixel.R(0, 0, 1024, 768)
+	canvas := pixelgl.NewCanvas(windowBounds)
+	return lisp.NewPrimitive(canvas), nil
 }
 
 func drawCanvas(args []lisp.SExpression) (lisp.SExpression, error) {
-    canvas := args[0].AsPrimitive().(*pixelgl.Canvas)
-    target := args[1].AsPrimitive().(pixel.Target)
-    canvas.Draw(target, pixel.IM)
-    return lisp.NewPrimitive(true), nil
+	canvas := args[0].AsPrimitive().(*pixelgl.Canvas)
+	target := args[1].AsPrimitive().(pixel.Target)
+	canvas.Draw(target, pixel.IM)
+	return lisp.NewPrimitive(true), nil
 }
